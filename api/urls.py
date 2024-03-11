36a62dfa4affc3ff03f46a9df7a50cbbc89f9941
Praveen Singh Rawat <68640765+praveenrawat2499@users.noreply.github.com>
2024-03-11 23:08:10 +0530

    Delete api/urls.py

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import AlertsLC, AlertsRUD
-
-urlpatterns = [
-    path('create/', AlertsLC.as_view(), name='create_reminder'),
-    path('alrud/', AlertsRUD.as_view(), name='rud_reminder'),
-]
